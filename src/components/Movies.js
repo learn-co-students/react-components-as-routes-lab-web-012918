4746a71f3a36da5d2102afbbb395da648484ecb2
allinthemind66 <r.nilon92@gmail.com>
2018-04-09 13:49:01 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,10 +2,26 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  console.log(movies)
   return (
-    <div>
-        {/*{code here}*/}
+
+  <div>
+    <h1>Movies Page</h1>
+    {movies.map(movie => {
+    return (
+      <div>
+      <h3>Name: {movie.title}</h3>
+      <p>Time: {movie.time}</p>
+      <p>Genres:</p>
+      <ul>
+        {movie.genres.map(genre => {
+          return <li key={genre}>{genre}</li>
+        })}
+      </ul>
+
     </div>
+    )
+  })}</div>
   );
 };
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,22 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
-    </div>
+    <div className='navbar'>
+    <ul>
+      <li>
+      <NavLink to="/">Home</NavLink>
+    </li>
+      <li>
+      <NavLink to="/movies">Movies</NavLink>
+    </li>
+      <li>
+      <NavLink to="/directors">Directors</NavLink>
+    </li>
+      <li>
+      <NavLink to="/actors">Actors</NavLink>
+    </li>
+    </ul>
+  </div>
   );
 };
 

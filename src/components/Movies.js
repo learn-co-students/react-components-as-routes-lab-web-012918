f0f3fa2d45f67827203db110adb39e654ca87ce1
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-04-09 14:49:05 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  const movieData = () => {
+    return movies.map(movie => {
+      return <div>
+        <h3>{movie.title}</h3>
+        <p>{movie.time}</p>
+        {movie.genres.map(genre => {
+          return <p>{genre}</p>
+        })}
+      </div>
+    })
+  }
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies</h1>
+        {movieData()}
     </div>
   );
 };

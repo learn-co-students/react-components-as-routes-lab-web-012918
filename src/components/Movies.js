c917d9fbc400b4bcf98f62cbd0d04d279f40b4ff
gilsunshine <gilsunshine@gmail.com>
2018-04-10 11:25:27 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  let mov = movies.map(movie => {
+    return (<div key={movie.name} className="movie">
+      {movie.title}
+      {movie.time}
+      {movie.genres}
+    </div>)
+  })
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {mov}
     </div>
   );
 };

f7ac32654a467a92e4b444cab3194fd335da7a78
github username <github email address>
2018-04-10 11:36:23 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,9 +4,19 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(director => {
+        return <div>
+        <h3>{director.name}</h3>
+        <ul>
+        {director.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+        </div>
+      })}
     </div>
   );
 }
 
 export default Directors
+
+// This component should render the text Directors Page, and make a new <div> for each director. The <div> should contain the director's name and an <ul> for each of their movies.
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { movies } from '../data';
 
+
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+    <h1>Movies Page</h1>
+      {movies.map( movie => {
+          return (<div>
+          <h3>{movie.title}</h3>
+          <h4>{movie.time}</h4>
+          <ul>
+          {movie.genres.map(genre => <li>{genre}</li>)}
+          </ul>
+          </div>)
+        })}
     </div>
   );
 };
 
 export default Movies;
+
+// This component should render the text Movies Page, and make a new <div> for each movie. The <div> should contain the movie's title, time and an <ul> for each genre.

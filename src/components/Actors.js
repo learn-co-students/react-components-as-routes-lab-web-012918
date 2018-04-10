9defbbad0ac6309ee04a7c318c407ab5d9af7bbf
jaf7 <janthonyfields@gmail.com>
2018-04-09 21:20:07 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,9 +4,18 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => 
+        <div>
+          <h3>{actor.name}</h3>
+          <h4>Movies:</h4>
+          <ul>
+            {actor.movies.map(movie => <li>{movie}</li>)}
+          </ul>
+        </div>)}
     </div>
   );
 };
 
 export default Actors;
+  
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,16 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => 
+          <div>
+            <h3>{movie.title}</h3>
+            <h4>Running Time: {movie.time}</h4>
+            <h4>Genres:</h4>
+            <ul>
+              {movie.genres.map(genre => <li>{genre}</li>)}
+            </ul>
+          </div>)}
     </div>
   );
 };
